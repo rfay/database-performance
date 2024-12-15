13e2f3b8df17b7ec5efe2a9cdbabf7140d78933d
Randy Fay <randy@randyfay.com>
2024-12-15 06:36:32 -0700

    initial information

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # database-performance
 Experimental database performance tools
+
+This repo is to explore database performance tests with DDEV.
+
+Importing large database has sometimes been an area of concern because they can take a long time and it's hard to understand why and what the differences are between DDEV versions and database types.
+
+Areas of exploration:
+* Creation and availability of large database dumps
+* Scripts to compare import performance across database types and DDEV versions
+* Scripts to compare snapshot restore performance
+
+
+## Available db dumps
+
+These dumps have no confidential data and are entirely generated using publicly available information.
+
+* [Drupal CMS db.sql.gz](https://drive.google.com/file/d/1eOTsh_lJ7cpGbVDkyu_kgderzh2DfWM3/view?usp=sharing)
+* [Drupal CMS MariaDB 10.11 DDEV snapshot](https://drive.google.com/file/d/1-UYSzfF_ybEFlLu6I4txbHGlWbiimo70/view?usp=sharing)
+
---
+++ scripts/create-large-db.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# This script helpfully curated via chatgpt attempts to build a huge database for import
+# Run it in the context of a fresh project of the correct database type and version
+# For bundles, it uses an early version of drupal-cms
+# This can be run multiple times, or have the variables changed to create more items.
+
+# Variables
+NUM_USERS=10000
+NUM_NODES=100000
+NUM_COMMENTED_NODES=100
+BUNDLES="case_study,page"
+DB_EXPORT_PATH=".tarballs/gen.sql.gz"
+
+ddev start
+
+# Enable required modules
+echo "Enabling Devel and Devel Generate modules..."
+ddev drush pm:enable devel devel_generate -y
+
+# Generate users
+echo "Generating $NUM_USERS users..."
+ddev drush devel-generate-users $NUM_USERS --roles="authenticated"
+
+# Generate nodes with comments
+echo "Generating $NUM_NODES nodes in bundles: $BUNDLES, with $NUM_COMMENTED_NODES nodes having comments..."
+ddev drush devel-generate-content --bundles=$BUNDLES $NUM_NODES $NUM_COMMENTED_NODES
+
+# Export the database
+echo "Exporting the database to $DB_EXPORT_PATH..."
+ddev export-db --file=$DB_EXPORT_PATH
+
+echo "Database export complete: $DB_EXPORT_PATH"
